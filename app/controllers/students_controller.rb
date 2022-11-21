554691422141a13e29a88fdd068212bce3922baa
Richard Ouma <richard.ouma@student.moringaschool.com>
2022-11-21 23:53:23 +0300

    Done!

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,16 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def grades
+    students = Student.find(params[:grades])
+    render json: students/grades
+  end
+
+
+  def show
+    students = Student.find_by(id: params[:id])
+    render json: students
+
+  end
+
 end
